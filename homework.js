2af3fc670ce9c3d665af5fbb51e6b6795bd09e6d
samkigs <91659509+samkigs@users.noreply.github.com>
2021-10-27 23:28:45 -0400

    another commit

---
+++ homework.js
@@ -0,0 +1,39 @@
+console.log("inafika")
+
+// dateControl.value = '2017-06-01';
+
+function validate() {
+
+    console.log("inafika function")
+    var dateControl = document.querySelector('input[type="date"]').value;
+    // var input = dateControl.value
+    // var dateEntered = new Date(dateControl);
+   // console.log(dateControl); //prints "2017-01"
+   // console.log(dateControl.valueAsNumber); // prin
+
+    console.log(dateEntered); // prin
+
+   // var year = dateEntered.getFullYear()
+  //  var month = dateEntered.getMonth()
+  //  var day = dateEntered.getDate()
+
+   // console.log(typeof year) + ' year ' + typeof month + 'month' + typeof day
+
+   // var yearString = year.toString()
+
+    // var CC = parseInt(yearString.slice(0, 2)
+   // var YY = parseInt(yearString.slice(2, 4)
+    // var MM = month
+  //  var DD = day
+
+ //   var dayOfTheWeek = parseInt((CC / 4) - 2*CC-1) + ((5+YY/4))
+
+    //console.log(typeof CC)
+    //console.log(Math.round(dayOfTheWeek))
+   // var d = parseInt((CC / 4 - 2 + CC - 1 + (5 + YY) / 4 + ))
+   // console.log(dayOfTheWeek);
+
+   // var name = ["wed", "thur", "fri", "sat", "sun", "mon", "tue"]
+
+   // console.log(name[dayOfTheWeek]);
+}
