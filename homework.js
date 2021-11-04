e5e78922aeec754fd4bfb87195171315da820bec
samkigs <91659509+samkigs@users.noreply.github.com>
2021-11-04 08:58:03 -0400

    another commit boi

--- homework.js
+++ homework.js
@@ -1,94 +1,36 @@
-let year = document.getElementbyId("year").value
+console.log("inafika")
 
-console.log(year)
-let date = document.getElementbyId("year")
+// dateControl.value = "2017-06-01";
 
-console.log(date)
+function validate (){
 
-function getDate(){
-   let date = document.getElementById("year").value
+   console.log("inafika function")
+   var dateControl = document.querySelector('input[type="date"]').value;
+   var input = dateControl.value
+   var dateEntered = new Date (input);
+   console.log(dateControl.value); // print
+   console.log(dateControl.valueAsNumber)
 
-   let newDate = new Date(date);
-   
-   let year = newDate.getFullYear()
-   let month = newDate.getMonth()
-   
-   
+   console.log(dateEntered); // prin
 
+   var year = dateEntered.getFullYear()
+   var month = dateEntered.getMonth()
+   var day = dateEntered.getDate()
 
-   console.log(newDate.getFullYear())
-   let cc = year.slice(0,2)
-   console.log(cc)
-}
-// get date input
-// get gender input
-// separate and apply the formular return digit number between -0-6
-// convert our akan names into array
-// male ['Kwasi', 'Kwabena', 'Kwaku']
-// female ['Kwasi', 'Kwabena', 'Kwaku']
-/* if male */
-//use male array
-// male[0] = "Kwasi"
-// female
-//use female array
-// female[0] = "Kwasi"
-//console.log("inafika")
+   console.log(typeof year + 'year' + typeof month + 'month' + typeof day + 'day')
 
-// dateControl.value = '2017-06-01';
+   var yearString = year.toString()
 
-function validate() {
+   var CC = parseInt (yearString.slice(0,2))
+   var YY = parseInt (yearString.slice(2,4))
+   var MM = month
+   var DD = day
 
-    console.log("inafika function")
-    //var dateControl = document.querySelector('input[type="date"]').value;
+   var dayOfTheWeek = parseInt((CC / 4) - 2*CC-1) + ((5+YY/4))
 
-    //get gender input
+   //console.log(typeof CC)
+   //console.log(Math.round(dayOfTheWeek))
+//  var d = parseInt((CC / 4) - 2*CC-1) + (5+YY/4) - 4 -
+   console.log(dayOfTheWeek);
 
-
-
-    // var input = dateControl.value
-    // var dateEntered = new Date(dateControl);
-   // console.log(dateControl); //prints "2017-01"
-
-
-   // console.log(dateControl.valueAsNumber); // prin
-
-    //console.log(dateEntered); // prin
-
-   // var year = dateEntered.getFullYear()
-  //  var month = dateEntered.getMonth()
-  //  var day = dateEntered.getDate()
-
-   // console.log(typeof year) + ' year ' + typeof month + 'month' + typeof day
-
-   // var yearString = year.toString()
-
-    // var CC = parseInt(yearString.slice(0, 2)
-   // var YY = parseInt(yearString.slice(2, 4)
-    // var MM = month
-  //  var DD = day
-
- //   var dayOfTheWeek = parseInt((CC / 4) - 2*CC-1) + ((5+YY/4))
-
-    //console.log(typeof CC)
-    //console.log(Math.round(dayOfTheWeek))
-   // var d = parseInt((CC/4) - 2*CC - 1) + ((5*YY/4) / 4 + ))
-   // console.log(dayOfTheWeek);
-
-   var MaleNames = ["wed", "thur", "fri", "sat", "sun", "mon", "tue"]
-   var femaleNames = ["wed", "thur", "fri", "sat", "sun", "mon", "tue"]
-
-   if (gender === "male"){
-      var akanName = MaleNames[dayOfTheWeek]
-      console.log(akanName)
-   }
-   else if (gender === "female"){
-      var femaleAkan = femaleNames[dayOfTheWeek]
-      console.log(femaleAkan)
-
-   }
-   else{
-      console.log("too bad to choose a gender")
-   }
-   // console.log(name[dayOfTheWeek]);
-   // alert (name[dayOfTheWeek]);
 }
