11cc199e39bb27f924688c18370f064f81a66430
samkigs <91659509+samkigs@users.noreply.github.com>
2021-11-04 19:51:18 +0300

    another good commit

--- homework.js
+++ homework.js
@@ -1,4 +1,4 @@
-console.log("inafika")
+// console.log("inafika")
 
 // dateControl.value = "2017-06-01";
 
@@ -6,31 +6,58 @@ function validate (){
 
    console.log("inafika function")
    var dateControl = document.querySelector('input[type="date"]').value;
-   var input = dateControl.value
-   var dateEntered = new Date (input);
-   console.log(dateControl.value); // print
-   console.log(dateControl.valueAsNumber)
 
-   console.log(dateEntered); // prin
+   //get gender input 
 
-   var year = dateEntered.getFullYear()
-   var month = dateEntered.getMonth()
-   var day = dateEntered.getDate()
+    var gender = "female"
 
-   console.log(typeof year + 'year' + typeof month + 'month' + typeof day + 'day')
+    var dateEntered = new Date(dateControl);  
+    console.log(dateControl); // prints "2017-06-01"
 
-   var yearString = year.toString()
+    calculateDayOfTheWeek (dateEntered)
 
-   var CC = parseInt (yearString.slice(0,2))
-   var YY = parseInt (yearString.slice(2,4))
-   var MM = month
-   var DD = day
+ }
 
-   var dayOfTheWeek = parseInt((CC / 4) - 2*CC-1) + ((5+YY/4))
+    function calculateDayOfTheWeek(dateEntered){
 
-   //console.log(typeof CC)
-   //console.log(Math.round(dayOfTheWeek))
-//  var d = parseInt((CC / 4) - 2*CC-1) + (5+YY/4) - 4 -
-   console.log(dayOfTheWeek);
+    var year = dateEntered.getFullYear()
+    var month = dateEntered.getMonth()
+    var day = dateEntered.getDate()
+    
+    console.log(typeof year + " year " + typeof month + " month " + typeof day + " day ")
+    
+    var yearString = year.toString()
+    
+    var CC = parseInt(yearString.slice(0,2))
+    var YY = parseInt(yearString.slice(2,4))
+    var MM = month
+    var DD = day
+    
+    var dayOfTheWeek = parseInt( ( (CC/4) -2*CC-1) + ((5*YY/4) )  + ((26*(MM+1)/10)) + DD ) % 7
+    
+    console.log(dayOfTheWeek);
 
-}
+
+ }
+
+ function getAkan(){
+    var maleNames = ("wed", "thur", "fri", "sat", "sun", "mon", "tue")
+    var femaleNames = ("jumatatu", "jumanne", "jumatano", "alhamisi", "ijumaa", "jumamosi", "jumapili")
+    
+    if (gender === "male"){
+        var akanName = maleNames[dayOfTheWeek]
+        console.log(akanName)
+        alert(akanName)
+    
+    }
+    else if(gender === "female"){
+        var femaleAkan = femaleNames [dayOfTheWeek]
+    
+        console.log(femaleAkan)
+        alert(femaleAkan)
+    }
+    else{
+        console.log("too bad choose a gender")
+    }
+
+ }
